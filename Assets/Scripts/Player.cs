47f59abc496d41ecfe345692e8085eae77376e81
hms2002 <hmm2002@naver.com>
2022-01-03 14:42:36 +0900

    타일맵 + 플레이어 무빙(애니메이션 포함)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float h;
+    float v;
+    public int speed;
+
+    Rigidbody2D rigid;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start(){
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //#1.플레이어 이동
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");  
+
+    
+        if(animator.GetInteger("hAxisRaw") != h){
+            animator.SetBool("isChange", true);
+            animator.SetInteger("hAxisRaw", (int)h);
+        }
+        else if(animator.GetInteger("vAxisRaw") != v){
+            animator.SetBool("isChange", true);
+            animator.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+            animator.SetBool("isChange", false);
+
+        if(h == 0 && v == 0)
+            animator.SetBool("GoIdle", true);
+        else
+            animator.SetBool("GoIdle", false);
+
+    }
+
+    private void FixedUpdate() {
+        //#1.플레이어 이동
+        rigid.velocity = new Vector2(h,v) * speed;
+    }
+}
