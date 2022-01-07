bd0026a284617f5bd3fda0bb8a01d667062d5258
MicholJackson <4097lsh@naver.com>
2022-01-07 15:59:12 +0900

    go to bossroom
    드가자

---
+++ Assets/Scripts/EnterTheBossRoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterTheBossRoom : MonoBehaviour
+{
+    public GameObject TextBox;
+    public Transform destination;
+    Player player;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Player reached the door");
+        player = other.GetComponent<Player>();
+
+        TextBox.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Player move away from the door");
+
+        TextBox.SetActive(false);
+    }
+
+    public void GoToPos()
+    {
+        player.gameObject.transform.position = destination.position;
+    }
+}
