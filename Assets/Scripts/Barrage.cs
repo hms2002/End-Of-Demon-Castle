8f8a9bfe6899ff079a75b7402382b934b2983ccd
nadoman354 <nadoman354@naver.com>
2022-01-10 16:27:29 +0900

    패턴1,탄막 시스템,최적화

---
+++ Assets/Scripts/Barrage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrage : MonoBehaviour
+{
+    public int Damage = 10;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        int Layer = collision.gameObject.layer;
+        if(Layer == 10)
+        {
+            collision.GetComponent<Player>().damaged(Damage);
+            gameObject.SetActive(false);
+        }
+        if(collision.gameObject.tag == "Border")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    public GameObject BarragePrefab;
+    GameObject[] Barrage;
+    GameObject[] targetPool;
+    void Awake()
+    {
+        Barrage = new GameObject[50];
+
+        Generate();
+    }
+
+    void Generate()
+    {
+        for(int index = 0; index < Barrage.Length; index++)
+        {
+            Barrage[index] = Instantiate(BarragePrefab);
+            Barrage[index].SetActive(false);   
+        }
+    }
+
+    public GameObject MakeObj(string type)
+    {
+        switch(type)
+        {
+            case "Barrage":
+                targetPool = Barrage;
+                break;
+        }
+
+        for (int index = 0; index < targetPool.Length; index++)
+        {
+            if (!targetPool[index].activeSelf)
+            {
+                targetPool[index].SetActive(true);
+                return targetPool[index];
+            }
+        }
+
+        return null;
+    }
+}
