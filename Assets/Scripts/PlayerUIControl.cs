8e1c20ca76ed9f908e65c66d080bc723a31a173d
MicholJackson <4097lsh@naver.com>
2022-01-14 15:34:33 +0900

    Retry버튼 색상, FireBreath 스프라이트 교체
    fireBreath 스프라이트 충돌 마스크 수정하다가 끝남

--- Assets/Scripts/PlayerUIControl.cs
+++ Assets/Scripts/PlayerUIControl.cs
@@ -10,6 +10,7 @@ public class PlayerUIControl : MonoBehaviour
     Animator playerAnimator;
     public GameObject canvas_dead; 
     public Image fadeImage_dead;
+    public Text retry;
     public GameObject retryButton;
 
     private void Start() {
@@ -34,14 +35,30 @@ public class PlayerUIControl : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
 
-        Color startColor = fadeImage_dead.color;
+        Color imageColor = fadeImage_dead.color;
 
         for (int i = 0; i < 200; i++)
         {
-            startColor.a += 0.01f;
-            fadeImage_dead.color = startColor;
+            imageColor.a += 0.01f;
+            fadeImage_dead.color = imageColor;
             yield return new WaitForSeconds(0.01f);
         }
         retryButton.SetActive(true);
+    }
+
+    public void MousePointEnter()
+    {
+        Color textColor = retry.color;
+        textColor.g -= 1;
+        textColor.b -= 1f;
+        retry.color = textColor;
+    }
+
+    public void MousePointExit()
+    {
+        Color textColor = retry.color;
+        textColor.g += 1;
+        textColor.b += 1f;
+        retry.color = textColor;
     }
 }
