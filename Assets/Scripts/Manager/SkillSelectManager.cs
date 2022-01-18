212da0b621e6fe8027a59e1bc0938a8b087c54ff
hms2002 <hmm2002@naver.com>
2022-01-18 17:08:51 +0900

    skillSlotEnd

---
+++ Assets/Scripts/Manager/SkillSelectManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillSelectManager : MonoBehaviour
+{
+    public GameObject player;
+    public SkillSlot[] skillSlot;
+    public List<Skill_ID> skillList = new List<Skill_ID>();
+    public GameObject slotParent;
+
+    public PlayerSkill playerSkill;
+    private void Awake() {
+        skillSlot = slotParent.GetComponentsInChildren<SkillSlot>();
+        playerSkill = FindObjectOfType<PlayerSkill>();
+    }
+
+    public void EndSelect()
+    {
+        while(skillList.Count > 0)
+        {
+            Destroy(skillList[0]);
+            skillList.RemoveAt(0);
+        }
+
+        for(int i = 0; i < skillSlot.Length; i++)
+        {
+            if(skillSlot[i].skill == null)
+            {
+                skillList.Add(player.AddComponent<Skill_ID>());
+                continue;
+            }
+                
+            int big = skillSlot[i].skill.ID/10;
+            switch(big)
+            {
+                case 1:
+                    skillList.Add(player.AddComponent<PlayerSkill_FireBreath>());
+                    break;
+                case 2:
+                
+                    break;
+                case 3:
+                
+                    break;
+                case 4:
+                
+                    break;
+                case 5:
+                
+                    break;
+                case 6:
+                
+                    break;
+                default:
+                    break;
+            }
+
+            if(i == 0)
+            {
+                playerSkill.rMouseButton = skillList[i].SkillOn;
+                skillList[i].skillKey = KeyCode.Mouse1;
+            }
+            else if(i == 1)
+            {
+                playerSkill.q = skillList[i].SkillOn;
+                skillList[i].skillKey = KeyCode.Q;
+            }
+            else if(i == 2)
+            {
+                playerSkill.e = skillList[i].SkillOn;
+                skillList[i].skillKey = KeyCode.E;
+            }
+            else if(i == 3)
+            {
+                playerSkill.r = skillList[i].SkillOn;
+                skillList[i].skillKey = KeyCode.R;
+            }
+            else if(i == 4)
+            {
+                playerSkill.leftShift = skillList[i].SkillOn;
+                skillList[i].skillKey = KeyCode.LeftShift;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerSkill/Skill_ID.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_ID : MonoBehaviour
+{
+    public int ID;
+    public KeyCode skillKey;
+
+    public virtual void SkillOn()
+    {
+
+    }
+}
