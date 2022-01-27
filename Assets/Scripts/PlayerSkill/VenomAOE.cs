0092361fc5e41da7be700c9842f9fd4c6e36c22f
nadoman354 <nadoman354@naver.com>
2022-01-27 16:04:02 +0900

    독병 스나이퍼 폭발 사운드 적용 스나이퍼 이펙트 적용

--- Assets/Scripts/PlayerSkill/VenomAOE.cs
+++ Assets/Scripts/PlayerSkill/VenomAOE.cs
@@ -9,9 +9,12 @@ public class VenomAOE : MonoBehaviour
     private float currentDamageTime;
     Animator anim;
     CapsuleCollider2D capsule;
+    AudioSource audioSource;
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        SoundManager.GetInstance().Play(audioSource, "Sound/PlayerSound/SkillSound/PoisonBottleBreak", 0.6f);
         capsule = gameObject.GetComponent<CapsuleCollider2D>();
         DamageTime = 0.5f;
         capsule.enabled = true;
