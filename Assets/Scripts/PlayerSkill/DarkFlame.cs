e2b4f916488f1c2df29d70a9abc0620c95a05815
hms2002 <hmm2002@naver.com>
2022-02-24 11:21:49 +0900

    dark

--- Assets/Scripts/PlayerSkill/DarkFlame.cs
+++ Assets/Scripts/PlayerSkill/DarkFlame.cs
@@ -71,7 +71,7 @@ public class DarkFlame : MonoBehaviour
             if (curTime > 2)
             {
                 curTime = 0;
-                player.damaged(10);
+                player.damaged(7);
             }
         }
     }
