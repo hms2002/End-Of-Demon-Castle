9d42b05b3d60aba78c5e10aa26a4969fe067d1ec
MicholJackson <4097lsh@naver.com>
2022-02-24 14:22:21 +0900

    독 장판 데미지 텀 증가

--- Assets/Scripts/PlayerSkill/VenomAOE.cs
+++ Assets/Scripts/PlayerSkill/VenomAOE.cs
@@ -18,7 +18,7 @@ public class VenomAOE : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         SoundManager.GetInstance().Play(audioSource, "Sound/PlayerSound/SkillSound/PoisonBottleBreak", 0.6f);
         capsule = gameObject.GetComponent<CapsuleCollider2D>();
-        DamageTime = 0.5f;
+        DamageTime = 1.0f;
         capsule.enabled = true;
         anim = GetComponent<Animator>();
         StartCoroutine("VenomEnd");
