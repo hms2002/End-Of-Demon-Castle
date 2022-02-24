7c7b31bc7079e2cc4ac486569f7762ff407816e7
MicholJackson <4097lsh@naver.com>
2022-02-24 17:51:32 +0900

    사망 사운드 수정

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -51,9 +51,9 @@ public class SoundManager : MonoBehaviour
 
     public void Dead()
     {
-        Destroy(root.transform.GetChild(1).gameObject);
+        Destroy(root.transform.GetChild(0).gameObject);
         _audioSources[(int)Define.Sound.Bgm].loop = false;
-        SoundManager.GetInstance().Play("Sound/LevelSound/DeadScene", 0.7f, Define.Sound.Bgm);
+        SoundManager.GetInstance().Play("Sound/LevelSound/DeadScene", 0.7f, Define.Sound.Effect);
     }
 
     public void Init()
