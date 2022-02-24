9a45ce5e657fd6cb4eb9367e0882c3011a435671
nadoman354 <nadoman354@naver.com>
2022-02-24 13:44:05 +0900

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -348,7 +348,7 @@ public class Player : MonoBehaviour
                 StartCoroutine("IChangeCanHit");
                 StartCoroutine("ShowDamaged");
                 player_hp -= damage;
-                SoundManager.GetInstance().Play("Sound/PlayerSound/A_PA", 0.5f);
+                SoundManager.GetInstance().Play("Sound/PlayerSound/A_PA", 0.3f);
                 dead();
             }
         }
