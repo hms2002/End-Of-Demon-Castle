d7fc8decc03d2f3fc0c65acbb4919df1a3d4bc97
hms2002 <hmm2002@naver.com>
2022-02-24 14:05:22 +0900

    shadow assult

--- Assets/Scripts/PlayerSkill/PlayerSkill_ShadowAssault.cs
+++ Assets/Scripts/PlayerSkill/PlayerSkill_ShadowAssault.cs
@@ -116,7 +116,7 @@ public class PlayerSkill_ShadowAssault : Skill_ID
         
         if(canLerp)
         {
-            for (float i = 0; i <= 100; i += 500 * Time.deltaTime)
+            for (float i = 0; i <= 100; i += 1000 * Time.deltaTime)
             {
                 if(i == 50 && i == 70 && i == 90)
                 {
